8f2c6b89ccd942754d24e9e0e361dc9dbd3d9171
mohcine elbah <mohcine.elbah@gmail.com>
2023-11-03 15:59:08 +0100

    adding settings to options

--- src/components/common/SideBar/userOptions/UserOptions.tsx
+++ src/components/common/SideBar/userOptions/UserOptions.tsx
@@ -1,10 +1,12 @@
 import './UserOptions.scss';
 import UserOption from './userOption/UserOption';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 
 export default function UserOptions() {
     return (
         <div className='userOptions_main'>
+            <UserOption userOptionName="Parametres" userOptionIcon={<SettingsRoundedIcon />} />
             <UserOption userOptionName="Deconnexion" userOptionIcon={<LogoutOutlinedIcon />} />
         </div>
     );
